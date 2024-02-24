a8a2b38c1702209b849c2fec891b1350e76d3e04
mooradian79 <martin.mooradian@gmail.com>
2024-02-24 09:15:29 -0500

    Added links to pages and articles.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  root "pages#home"
+
+  get 'about', to: 'pages#about'
+  
+  resources :articles
 end
